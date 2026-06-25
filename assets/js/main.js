c066b0b15c7f9c558dfed90114a037d785ce266f
UnGooD <andreimg500@gmail.com>
2026-06-25 15:39:31 +0800

    Подключён Web3Forms ключ - заявки идут на почту

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,7 +132,7 @@
   }
 
   /* ---------- Отправка форм (Web3Forms) ---------- */
-  var WEB3FORMS_KEY = 'ВАШ_ACCESS_KEY'; // получить бесплатно на web3forms.com
+  var WEB3FORMS_KEY = '29946204-51c2-465b-acde-07738ed68806'; // Web3Forms → andreimg500@gmail.com
   document.querySelectorAll('form[data-lead]').forEach(function (form) {
     form.addEventListener('submit', async function (e) {
       e.preventDefault();
